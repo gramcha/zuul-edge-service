7c1d5e9d4f787b776b982e3197f24b92c700fba1
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-10-29 18:54:15 +0530

    excluding default spring auto config to avoid default username and pwd

--- src/main/java/com/gramcha/zuuledgeservice/ZuulEdgeServiceApplication.java
+++ src/main/java/com/gramcha/zuuledgeservice/ZuulEdgeServiceApplication.java
@@ -2,12 +2,15 @@ package com.gramcha.zuuledgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 @EnableEurekaClient     // Register this service as uerka client to eureka server
 @EnableZuulProxy        // Enable Zuul
+@ComponentScan(basePackages = "com.gramcha.*")
 public class ZuulEdgeServiceApplication {
 
 	public static void main(String[] args) {
