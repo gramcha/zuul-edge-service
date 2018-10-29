f6fe58710efcc6accbef405f303c40f2ed433439
Ganeshkumar Ramachandran <haiganeshkumar@gmail.com>
2018-10-29 18:53:41 +0530

    token validation at edge server

---
+++ src/main/java/com/gramcha/config/SecurityTokenConfig.java
@@ -0,0 +1,61 @@
+package com.gramcha.config;
+
+import com.gramcha.services.JwtTokenAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by gramachandran on 29/10/18.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityTokenConfig  extends WebSecurityConfigurerAdapter {
+//    @PostConstruct
+//    public void init(){
+//        System.out.println("------SecurityTokenConfig-----");
+//    }
+    @Autowired
+    private JwtConfig jwtConfig;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        System.out.println("-----configure()----");
+        http
+                .csrf().disable()
+                // make sure we use stateless session; session won't be used to store user's state.
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // handle an authorized attempts
+                .exceptionHandling().authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+                .and()
+                // Add a filter to validate the tokens with every request
+                .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
+                // authorization requests config
+                .authorizeRequests()
+                // allow all who are accessing "auth" service
+                .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
+                // must be an admin if trying to access admin area (authentication is also required here)
+                .antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
+                // Any other request must be authenticated
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public JwtConfig jwtConfig() {
+        return new JwtConfig();
+    }
+}
